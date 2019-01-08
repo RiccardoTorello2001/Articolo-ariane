e4dc3f6a3947bf1f55ab97ad4ba4168bd173c1e2
Riccardo Torello <rikitorello2001@gmail.com>
2019-01-08 17:21:08 +0100

    spiego in che cosa consiste il programma e l'errore dovuto al calcolo

---
+++ Ariane/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ariane
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//il calcolo che rappresentiamo è un calcolo errato usato dagli arabi nel tentativo di colpire lo scud ma un piccolo di spazio riusci a deviarlo e far morire 28 soldati.
+			//il calcolo serviva per trasformare ii tempo da millisecondi a secondi
+			//si prende il numero e si moltiplica per 1/10, la rappresentazione binaria di 1/10 è infinita e la rappresenteremo con x.
+			int x = 1 / 10;
+			//i registri avevano una lunghezza di 24 bit,utilizzando un bit per il segno la rappresentazione fu troncata alla ventitresima cifra dopo la virgola.
+		}
+	}
+}
